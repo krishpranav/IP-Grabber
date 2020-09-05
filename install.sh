b2361c15da8bdbced137d4ab79bab420efc17d42
krishpranav <krisna.pranav@gmail.com>
2020-09-05 08:48:52 +0530

    first commit

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+
+clear
+echo "UPDATING"
+sudo apt-get update
+clear
+echo "INSTALLING PHP"
+sudo apt-get install php
+clear
+echo "INSTALLING CURL"
+sudo apt-get install curl
+clear
+echo "COMPLETED INSTALLING"
